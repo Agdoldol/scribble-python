61a063b65351a5f2efce0fd94954f148f691dd8f
mosiclab <aghj@gmail.com>
2024-09-13 15:57:19 +0900

    fail...

--- kcm_python_test/1. About string.py
+++ kcm_python_test/1. About string.py
@@ -1,5 +1,5 @@
 #What is string?
-honghapp = 'delcious' #this is the string
+honghapp = 'is delcious' #this is the string
 mosiclab = "is using for making pasta" #this is also string
 num = 163.163163163
 #so we can understand the string as a sequence of characters that is treated as a single item
@@ -15,4 +15,8 @@ print(int(num)+num)
 #print(int(num)+mosiclab) ---> this is error!
 print(mosiclab*int(num))
 print(str(num)+mosiclab)
-#so you can guess string can be calculate by its type
+#so you can guess string can be calculated by its type
+
+#if you want to handle the string, you must know 'postion of a character' identified with the numbers
+print("I",honghapp[-6],mosiclab[10],"v","e","y",honghapp[8],"u")
+#as you can see 21st code, 'is delicious' code has start with 0 identified charecter number and it can also see as -1 to~ if it is count its end
--- kcm_python_test/3. string test.py
+++ kcm_python_test/3. string test.py
@@ -2,3 +2,7 @@ stri1 = "python is fun"
 stri2 = 'is false'
 print(stri1)
 print(stri2)
+
+input(put number:)
+print(input)
+input(hello number)
